5a38c65a871a785cbc17f9a6caf0772cdc232cc3
Jean Moissa <48718334+jeanmoissa@users.noreply.github.com>
2024-10-17 22:58:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (request, response) => {
     return response.json({ message: 'Server is up!' });
+});
+
+app.get('/atualizou', (request, response) => {
+    return response.json({ message: 'Atualizou mesmo!' });
+});
+
+app.get('/teste', (request, response) => {
+    return response.json({ name,date });
 });
 
 app.listen(3333);
